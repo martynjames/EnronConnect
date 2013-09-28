033e573a5ed7e8d5e5948f1349bf5bb8c6640eeb
Martyn James <martyn@hopper.com>
2013-09-28 15:25:30 -0400

    First page in place

--- app/helpers/connections_helper.rb
+++ app/helpers/connections_helper.rb
@@ -1,2 +1,10 @@
 module ConnectionsHelper
+  def all_user_options
+  	options_for_select all_emails
+  end
+
+  def all_emails
+  	[['martyn@hopper.com', 'martyn@hopper.com'], ['pxu@carbonite.com', 'pxu@carbonite.com']]
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ EnronConnect::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'connections#index'
 
   # See how all your routes lay out with "rake routes"
 
