f0848cc4c25c6cfa0a788b810a2211eaa0645b36
Martyn James <martyn@hopper.com>
2013-09-28 15:05:16 -0400

    App Skeleton

---
+++ app/models/index_access.rb
@@ -0,0 +1,71 @@
+require 'hashie/mash'
+require 'elasticsearch'
+
+class ESO
+  def initialize(incoming_hash)
+    @wrapped = Hashie::Mash.new(incoming_hash)
+  end
+
+  def es_object
+    @wrapped._source
+  end
+
+  def es_information
+    return @info unless @info.nil?
+
+    @info = @wrapped.clone
+    @info["_source"] = nil
+
+    @info
+  end
+
+end
+
+class Enron
+  @@es = Elasticsearch::Client.new hosts: ['cluster-7-slave-00.sl.hackreduce.net:9200']
+  
+  def self.get(hash_args)
+    query_hash = hash_args.merge({:index =>"enron"})
+    @@es.get query_hash
+  end
+
+  def self.get_source(hash_args)
+    query_hash = hash_args.merge({:index =>"enron"})
+    @@es.get_source query_hash
+  end
+
+  def self.search(hash_args)
+    query_hash = hash_args.merge({:index => "enron"})
+    @@es.search query_hash
+  end
+
+  def self.query(query_args)
+    self.search({:body => query_args})
+  end
+end
+
+class QB
+  def self.match(args)
+    {:query => {:match => args}}
+  end
+
+  #def self.suggest(text)
+  #end
+
+  def self.wildcard(args)
+    {:query => {:wildcard => args}}
+  end
+end
+
+#the_mail = Enron.get_source({:id => 'Dk87jMrmSPuhwzk0K-tuCQ'})
+#puts the_mail["To"]
+
+#from rex.shelby@enron.com
+#query_from = {:query => {:match => {:From => 'kenneth.lay@enron.com'}}}
+#query_from = QB.match({:From => 'kenneth.lay@enron.com'})
+#query_from = QB.wildcard({:content => '*chosen to leave Enron*'})
+#puts query_from
+#query_from = {:query => {:match_all => {}}}
+# look for term 
+#x = Hashie::Mash.new(Enron.query(query_from))
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnronConnect::Application.config.secret_token = 'cc52d45f4a4a1df0ac519f5a42912dc8f01bc2941a3e839fcc4faf57f814489b6d56fbe98892cd19ff69d7297354793bf82900aa8d9ebe1af559cf37ac4085b7'
